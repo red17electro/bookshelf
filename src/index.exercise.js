2a29da4a46d870103986f96ad802a8156a3bb1a3
red17electro <red17electro@gmail.com>
2022-07-22 15:41:26 +0200

    upd: exercise 1;
    Signed-off-by: red17electro <red17electro@gmail.com>

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,93 @@
-// 🐨 you'll need to import react and createRoot from react-dom up here
+import * as React from 'react'
+import {createRoot} from 'react-dom/client'
+import {Logo} from 'components/logo'
+import '@reach/dialog/styles.css'
+import {Dialog} from '@reach/dialog'
+import {VisuallyHidden} from '@reach/visually-hidden'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+const App = () => {
+  const [openModal, setOpenModal] = React.useState('none')
+  const handleLogin = () => {
+    setOpenModal('login')
+  }
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+  const handleRegister = () => {
+    setOpenModal('register')
+  }
 
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+  const handleSubmitLoginForm = ({username, password}) => {
+    console.log(username, password)
+    alert('login form submitted with data: ' + username + ' ' + password)
+  }
+
+  return (
+    <>
+      <Logo></Logo>
+      <CustomDialog isOpen={openModal === 'login'} setOpenModal={setOpenModal}>
+        <LoginForm onSubmit={handleSubmitLoginForm} buttonText="Login" />
+      </CustomDialog>
+      <CustomDialog
+        isOpen={openModal === 'register'}
+        setOpenModal={setOpenModal}
+        caption="Register"
+      ></CustomDialog>
+      <button role="button" onClick={handleLogin}>
+        Login
+      </button>
+      <button role="button" onClick={handleRegister}>
+        Register
+      </button>
+    </>
+  )
+}
+
+const CustomDialog = ({isOpen, setOpenModal, children}) => {
+  const close = () => {
+    setOpenModal('none')
+  }
+  return (
+    <Dialog isOpen={isOpen} onDismiss={close}>
+      <button className="close-button" onClick={close}>
+        <VisuallyHidden>Close</VisuallyHidden>
+        <span aria-hidden>×</span>
+      </button>
+      {children}
+    </Dialog>
+  )
+}
+
+const LoginForm = ({onSubmit, buttonText}) => {
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  return (
+    <div>
+      <p>Login form</p>
+      <label htmlFor="username">Username</label>
+      <input
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+        type="text"
+      />
+      <label htmlFor="username">Password</label>
+      <input
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        type="password"
+      />
+      <button
+        role="button"
+        type="submit"
+        onClick={e => {
+          e.preventDefault()
+          onSubmit({username, password})
+        }}
+      >
+        {buttonText}
+      </button>
+    </div>
+  )
+}
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+export {root}
