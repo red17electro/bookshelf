95f56e75d779d9677c0ccecbe487b46aff1f0d87
red17electro <red17electro@gmail.com>
2022-07-27 16:33:52 +0200

    upd: custom headers;
    Signed-off-by: red17electro <red17electro@gmail.com>

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,10 +1,17 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(endpoint, {token, ...customConfig} = {}) {
+function client(
+  endpoint,
+  {token, data, headers: customHeaders, ...customConfig} = {},
+) {
   const config = {
-    method: 'GET',
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
+      'Content-Type': data ? 'application/json' : undefined,
       Authorization: token ? `Bearer ${token}` : undefined,
+      ...customHeaders,
     },
     ...customConfig,
   }
@@ -13,6 +20,10 @@ function client(endpoint, {token, ...customConfig} = {}) {
     const data = await response.json()
     if (response.ok) {
       return data
+    } else if (response.status === 401) {
+      await auth.logout()
+      window.location.assign(window.location)
+      return Promise.reject(data)
     } else {
       return Promise.reject(data)
     }
